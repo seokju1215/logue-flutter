cdbf2eee80cae409207a22830fba8e8b39c2aed0
홍석주 <hsj202201@gmail.com>
2025-04-14 01:09:22 +0900

    feat: AppColors class 정의 (#1)

---
+++ lib/core/themes/app_colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+
+  static const Color black900= Color(0xFF191A1C);
+  static const Color black500 = Color(0xFF858585);
+  static const Color black300 = Color(0xFFD9D9D9);
+  static const Color black100 = Color(0xFFFAFAFA);
+  static const Color white500 = Color(0xFFFFFFFF);
+  static const Color blue500 = Color(0xFF0055FF);
+  static const Color red500 = Color(0xFFFF0000);
+
+
+
+
+
+
+}
---
+++ lib/core/themes/text_theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+final TextTheme appTextTheme = const TextTheme(
+  headlineLarge: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+  bodyMedium: TextStyle(fontSize: 16, color: Colors.black87),
+  labelLarge: TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
+);
