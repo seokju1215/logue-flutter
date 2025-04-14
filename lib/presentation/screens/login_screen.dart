cc8f7a71449f9c06eac5dcaca1816a77ac1abd58
홍석주 <hsj202201@gmail.com>
2025-04-14 22:50:45 +0900

    feat. 로그인 화면 구현 (#3)

--- lib/presentation/screens/login_screen.dart
+++ lib/presentation/screens/login_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:logue/domain/usecases/login_with_google.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:logue/core/themes/app_colors.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -26,11 +28,36 @@ class LoginScreen extends StatelessWidget {
     return Scaffold(
       body: Center(
         child: Column(
-          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            ElevatedButton(
-              onPressed: () => _login(context),
-              child: const Text("Google로 로그인"),
+            Expanded(
+              child: Center(
+                child: SvgPicture.asset(
+                  'assets/logue_logo_with_text.svg', // SVG 파일 경로
+                  height: 94,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 150.0), // 버튼 아래에 간격 추가
+              child: OutlinedButton(
+                onPressed: () => _login(context),
+                style: OutlinedButton.styleFrom(
+                  primary: Colors.black, // 텍스트 색상
+                  side: BorderSide(color: AppColors.black500, width: 1), // 테두리 색상
+                  padding: EdgeInsets.symmetric(vertical: 12, horizontal: 10),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5), // border-radius 설정
+                  ),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text("Google로 3초 만에 로그인하기"),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
