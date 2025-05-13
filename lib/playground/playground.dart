6df93c34466284e7de5140641c27d27f81612670
홍석주 <hsj202201@gmail.com>
2025-05-13 21:19:59 +0900

    feat. 책 3개 선택 화면 디자인 변경 반영 (#11)

--- lib/playground/playground.dart
+++ lib/playground/playground.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:logue/presentation/screens/signup/term_screen.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:logue/presentation/screens/signup/select_3books_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env"); // ✅ .env 등록 추가
+
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: TermsScreen() ,
+    home: Select3BooksScreen(),
   ));
 }
