a249bf9e8adfd7d9e45d3a197a313653bcc57de4
홍석주 <hsj202201@gmail.com>
2025-07-30 18:42:39 +0900

    fix. 배너는 이미지만 들어가도록

--- lib/core/widgets/banner/banner_slider.dart
+++ lib/core/widgets/banner/banner_slider.dart
@@ -40,48 +40,9 @@ class _BannerSliderState extends State<BannerSlider> {
                   final url = banner['target_url'] as String?;
                   if (url != null) _launchURL(url);
                 },
-                child: Stack(
-                  children: [
-                    Positioned.fill(
-                      child: Image.network(
-                        banner['image_url'],
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    Positioned(
-                      left: 0,
-                      right: 0,
-                      bottom: 0,
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 22,
-                          vertical: 10,
-                        ),
-                        color: Colors.black.withOpacity(0.5),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Expanded(
-                              child: Text(
-                                (banner['title'] ?? '')
-                                    .toString()
-                                    .replaceAll(r'\n', '\n'), // <-- 핵심 포인트
-                                style: const TextStyle(
-                                  color: AppColors.white500,
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                                maxLines: 3,
-                                overflow: TextOverflow.ellipsis,
-                                softWrap: true,
-                              ),
-                            ),
-                            const Icon(Icons.chevron_right, color: Colors.white, size: 40,),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
+                child: Image.network(
+                  banner['image_url'],
+                  fit: BoxFit.cover,
                 ),
               );
             },
