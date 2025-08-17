ac888a1ad3670f1ed4a2178c8a34bdb28b3c16b2
홍석주 <hsj202201@gmail.com>
2025-08-17 16:48:01 +0900

    fix. other_profile_screen의 공유 버튼 변경 및 홈의 팔로잉 탭 멘트 변경

--- lib/presentation/screens/home/home_following_tab.dart
+++ lib/presentation/screens/home/home_following_tab.dart
@@ -130,7 +130,7 @@ class _HomeFollowingTabState extends State<HomeFollowingTab> {
             child: Transform.translate(
               offset: AppConstants.getCenterOffset(context),
               child: const Text(
-                "친구를 팔로우해 서로의 인생 책을 공유해보세요",
+                "친구를 팔로우해 인생 책을 공유해보세요.",
                 style: TextStyle(fontSize: 12, color: AppColors.black500),
                 textAlign: TextAlign.center,
               ),
