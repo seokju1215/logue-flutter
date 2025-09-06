6c12a7e69f003ee79086c9129d344e8a24d02346
홍석주 <hsj202201@gmail.com>
2025-09-06 16:01:40 +0900

    fix. 프로필의 대표 탭 책 짤림 형상 해결

--- lib/presentation/screens/profile/profile_screen.dart
+++ lib/presentation/screens/profile/profile_screen.dart
@@ -241,7 +241,7 @@ class ProfileScreenState extends State<ProfileScreen> with WidgetsBindingObserve
       const extraSpace = 50.0;
       const minHeightFor3Rows = (bookHeight * 3) + (rowSpacing * 2) + extraSpace;
       
-      return (baseHeight - headerHeight).clamp(minHeightFor3Rows, baseHeight * 0.9);
+      return (baseHeight - headerHeight).clamp(minHeightFor3Rows, baseHeight) +15;
     }
   }
 
